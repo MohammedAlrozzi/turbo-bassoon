09306dbb3b134cb0c3b0c116ae059e4a511d59b3
Mohammed Al-Rozzi <alrozzi@protonmail.com>
2023-05-03 00:53:44 +0100

    remove key

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
 	const callAPI = async () => {
   try {
-    const token = '0ef4f134-3396-4cda-aa85-5e5aebc4fc0f265b465f-0a98-43f2-b0d6-7df4e233465a';
+    const token = 'api-key';
 
     const res = await fetch(
       `https://api.toshl.com/me`,
